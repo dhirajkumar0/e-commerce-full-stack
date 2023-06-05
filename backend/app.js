c036666c8fcdb9e92dac33f0c8de83443ec9918b
Dhiraj <kumardhiraj259@gmail.com>
2023-06-05 11:38:08 +0530

    deploy

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://dkshopo.vercel.app',
   credentials: true
 }));
 
