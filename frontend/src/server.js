2682761fef7301eb7610ee827dd89efdd8d3a695
Dhiraj <kumardhiraj259@gmail.com>
2023-06-05 12:14:56 +0530

    connect to banckend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://shopobackend.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://shopobackend.vercel.app";
